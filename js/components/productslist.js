2e3dec4733e5516c23f3954abf9f4ecd98ad4f66
unknown <ing.ramirozs@gmail.com>
2022-08-29 11:30:23 -0500

    creamos componente para llenar array con nombre de productos en el Dom

---
+++ js/components/productslist.js
@@ -0,0 +1,30 @@
+
+const div_gadgets = document.querySelectorAll(".gadget");
+const Products_gadgets = []; 
+ div_gadgets.forEach(e => { 
+  Products_gadgets.push(e.getElementsByTagName('p')[0].textContent);
+  return Products_gadgets;
+})
+
+
+const div_consolas = document.querySelectorAll(".consolas");
+const Products_consolas = []; 
+  div_consolas.forEach(e => { 
+  Products_consolas.push(e.getElementsByTagName('p')[0].textContent);
+  return Products_consolas;
+})
+
+
+const div_diverses = document.querySelectorAll(".diverses");
+const Products_diverses = []; 
+  div_diverses.forEach(e => { 
+  Products_diverses.push(e.getElementsByTagName('p')[0].textContent);
+  return Products_diverses;
+})
+
+export function loadproducts(){
+const Products = Products_gadgets.concat(Products_consolas,Products_diverses);
+return Products;
+}
+
+console.log(loadproducts())
--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 
+import {ChangeItem,selectItem} from "./components/autocomplete.js"
 
+const input = document.querySelector(".search_input");
+input.addEventListener("input",ChangeItem,selectItem);
+
+
+
+
+
+//this is a list with datalist and option test
 /*const producto = Listaproductos.forEach(element => {
    const data_list = document.getElementById("datos").getElementsByTagName("option");
    const a = `<option value="${element}"></option>`
@@ -15,11 +24,3 @@ let test = Listaproductos.forEach(element =>{
     element_datalist.appendChild(element_option);
     ;
 });*/
-
-import {ChangeItem,selectItem} from "./components/autocomplete.js"
-
-const input = document.querySelector(".search_input");
-input.addEventListener("input",ChangeItem,selectItem);
-
-
-
