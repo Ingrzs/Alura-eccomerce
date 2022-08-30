bc639be4c3fa91688db134504aa2a75f0202cd10
unknown <ing.ramirozs@gmail.com>
2022-08-30 12:49:35 -0500

    creamos clase y vaiarblae para botton_search

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
-
 import {ChangeItem,selectItem} from "./components/autocomplete.js"
 
 const input = document.querySelector(".search_input");
 input.addEventListener("input",ChangeItem);
 
-
-
 const elements_li = document.querySelector(".divlist");
 elements_li.addEventListener("click",selectItem);
 
-
+const button_search = document.querySelector(".button_search")
 
 
 
