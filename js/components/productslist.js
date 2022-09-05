1b38e2d84e5c22356c2f94d6b4bbdca5f6316d51
unknown <ing.ramirozs@gmail.com>
2022-09-04 23:36:10 -0500

    refactorizamos

--- js/components/productslist.js
+++ js/components/productslist.js
@@ -1,28 +1,21 @@
-
 const div_gadgets = document.querySelectorAll(".gadget");
-const Products_gadgets = []; 
- div_gadgets.forEach(e => { 
-  Products_gadgets.push(e.getElementsByTagName('p')[0].textContent);
-  return Products_gadgets;
-})
-
-
 const div_consolas = document.querySelectorAll(".consolas");
-const Products_consolas = []; 
-  div_consolas.forEach(e => { 
-  Products_consolas.push(e.getElementsByTagName('p')[0].textContent);
-  return Products_consolas;
-})
-
-
 const div_diverses = document.querySelectorAll(".diverses");
-const Products_diverses = []; 
-  div_diverses.forEach(e => { 
-  Products_diverses.push(e.getElementsByTagName('p')[0].textContent);
-  return Products_diverses;
-})
 
-export function loadproducts(){
-const Products = Products_gadgets.concat(Products_consolas,Products_diverses);
-return Products;
+let productsgadgets =[]
+let productsconsolas=[]
+let productsdiverses =[]
+
+function llenandoproductos(v,array){
+  v.forEach(e => array.push(e.getElementsByTagName('p')[0].textContent))
+  return array;
 }
+
+
+productsgadgets = llenandoproductos(div_gadgets,productsgadgets);
+productsconsolas = llenandoproductos(div_consolas,productsconsolas);
+productsdiverses =llenandoproductos(div_diverses,productsdiverses);
+
+export const totalproducts = productsgadgets.concat(productsconsolas,productsdiverses)
+console.log(totalproducts)
+
