a233f99665942ffb3c807aa4c7c427f57c4d5385
unknown <ing.ramirozs@gmail.com>
2022-10-01 01:05:07 -0500

    importamos nuevame las funciones para el searchbar

--- js/script.js
+++ js/script.js
@@ -1,29 +1,25 @@
-/*import { ProductosTotales } from "./components/Products.js";
-
-import {ChangeItem,selectItem} from "./components/autocomplete.js"
+/*
 import{FormularioNav, Openpage} from "./components/Formnav.js"*/
-import { Gadgets,Consolas,Diversos } from "./components/Products.js";
+import { Gadgets, Consolas, Diversos } from "./components/Products.js";
+import { ChangeItem, selectItem } from "./components/Autocomplete.js";
 
 const Items_gadgets = document.querySelector(".Items_gadgets");
 const Items_consolas = document.querySelector(".Items_consolas");
-const Items_diversos = document.querySelector(".Items_diversos")
+const Items_diversos = document.querySelector(".Items_diversos");
 Gadgets(Items_gadgets);
 Consolas(Items_consolas);
 Diversos(Items_diversos);
 
-
 const input = document.querySelector(".search_input");
-input.addEventListener("input",ChangeItem);
+input.addEventListener("input", ChangeItem);
 
 const elements_li = document.querySelector(".divlist");
-elements_li.addEventListener("click",selectItem);
+elements_li.addEventListener("click", selectItem);
 
-
+/*
 const button_search = document.querySelector(".button_search");
 button_search.addEventListener("click",FormularioNav)
-
-
-
+*/
 
 //this is a list with datalist and option test
 /*const producto = Listaproductos.forEach(element => {
