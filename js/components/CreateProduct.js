ef51937cf8d1cef07c0ac1264f8b2c500da3de71
unknown <ing.ramirozs@gmail.com>
2022-10-19 16:38:56 -0500

    add function create product

---
+++ js/components/CreateProduct.js
@@ -0,0 +1,10 @@
+export const CreateProduct = (object) => {
+    let Item = document.createElement("div");
+    Item.classList.add("Item");
+    Item.innerHTML =
+      `<img src=${object.img}>` +
+      `<p>${object.nombre}</p>` +
+      `<p>$${object.precio}</p>` +
+      `<a href=#>${object.ver}</a>`;
+    return Item;
+  };
