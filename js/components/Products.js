4fb4dc4fdde7e6bc5eabf4623c0c0d5c92b146b8
unknown <ing.ramirozs@gmail.com>
2022-10-22 21:08:23 -0500

    add id product

--- js/components/Products.js
+++ js/components/Products.js
@@ -4,41 +4,17 @@ import {
   ProductsDiversos,
 } from "../../Services/data.js";
 
-import { CreateProduct } from "./Createproduct.js";
-
-export const Gadgets = (v) => {
-  ProductsGadgets.forEach((product) => {
-    let Itemproduct = CreateProduct(product);
-    v.appendChild(Itemproduct);
-  });
-};
-
-export const Consolas = (v) => {
-  ProductsConsolas.forEach((product) => {
-    let Itemproduct = CreateProduct(product);
-    v.appendChild(Itemproduct);
-  });
-};
-
-export const Diversos = (v) => {
-  ProductsDiversos.forEach((product) => {
-    let Itemproduct = CreateProduct(product);
-    v.appendChild(Itemproduct);
-  });
-};
-
-
 export const Products = () => {
   let Gadgets = Object.entries(ProductsGadgets).map(([key, value]) => {
     return value;
   });
 
   let Consolas = Object.entries(ProductsConsolas).map(([key, value]) => {
-    return value
+    return value;
   });
 
   let Diversos = Object.entries(ProductsDiversos).map(([key, value]) => {
-    return value
+    return value;
   });
 
   const Products = Gadgets.concat(Consolas, Diversos);
