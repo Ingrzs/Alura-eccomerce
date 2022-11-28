a565af3984f25c04a786da82942d908241f3e740
unknown <ing.ramirozs@gmail.com>
2022-11-27 21:40:44 -0600

    update data

--- Services/data.js
+++ Services/data.js
@@ -1,136 +1,147 @@
 export const ProductsGadgets = [
-    {
-      id:1,
-      img: "https://img.freepik.com/premium-vector/black-laptop-computer-keyboard-template-isolated-white_53562-7277.jpg?w=740",
-      nombre: "Teclado",
-      precio: 345,
-      ver: "ver",
-    },
-    {
-      id:2,
-      img: "https://img.freepik.com/free-vector/computer-mouse-with-wire_1308-70619.jpg?w=2000",
-      nombre: "Mouse",
-      precio: 640,
-      ver: "ver",
-    },
-    {
-      id:3,
-      img: "https://img.freepik.com/free-photo/tv-with-wide-screen_144627-12169.jpg?w=740&t=st=1661876653~exp=1661877253~hmac=eda98456ec81faba0025eb3c33e0da9535cdbe6e3d3305f776076e2f6e74e858",
-      nombre: "Monitor",
-      precio: 3500,
-      ver: "ver",
-    },
-    {
-      id:4,
-      img: "https://img.freepik.com/premium-photo/black-ssd_253658-580.jpg?w=740",
-      nombre: "Disco Duro",
-      precio: 1000,
-      ver: "ver",
-    },
-    {
-      id:5,
-      img: "https://www.alugacenter.com/wp-content/uploads/2019/03/Captura-de-pantalla-2019-03-25-a-las-16.39.17.png",
-      nombre: "Lampara",
-      precio: 350,
-      ver: "ver",
-    },
-    {
-      id:6,
-      img: "https://img.freepik.com/free-photo/home-appliance-seat-interior-ergonomic-sign_1172-512.jpg?w=360&t=st=1661877001~exp=1661877601~hmac=a633d4732e2cc35bdb60184fc85004f473bc9315297e12a5fa0c8df82d749fe7",
-      nombre: "Silla gamer",
-      precio: 5000,
-      ver: "ver",
-    },
-  ];
+  {
+    id: 1,
+    img: "https://img.freepik.com/premium-vector/black-laptop-computer-keyboard-template-isolated-white_53562-7277.jpg?w=740",
+    nombre: "Teclado",
+    precio: 345,
+    ver: "ver",
+  },
+  {
+    id: 2,
+    img: "https://img.freepik.com/free-vector/computer-mouse-with-wire_1308-70619.jpg?w=2000",
+    nombre: "Mouse",
+    precio: 640,
+    ver: "ver",
+  },
+  {
+    id: 3,
+    img: "https://img.freepik.com/free-photo/tv-with-wide-screen_144627-12169.jpg?w=740&t=st=1661876653~exp=1661877253~hmac=eda98456ec81faba0025eb3c33e0da9535cdbe6e3d3305f776076e2f6e74e858",
+    nombre: "Monitor",
+    precio: 3500,
+    ver: "ver",
+  },
+  {
+    id: 4,
+    img: "https://img.freepik.com/premium-photo/black-ssd_253658-580.jpg?w=740",
+    nombre: "Disco Duro",
+    precio: 1000,
+    ver: "ver",
+  },
+  {
+    id: 5,
+    img: "https://www.alugacenter.com/wp-content/uploads/2019/03/Captura-de-pantalla-2019-03-25-a-las-16.39.17.png",
+    nombre: "Lampara",
+    precio: 350,
+    ver: "ver",
+  },
+  {
+    id: 6,
+    img: "https://img.freepik.com/free-photo/home-appliance-seat-interior-ergonomic-sign_1172-512.jpg?w=360&t=st=1661877001~exp=1661877601~hmac=a633d4732e2cc35bdb60184fc85004f473bc9315297e12a5fa0c8df82d749fe7",
+    nombre: "Silla gamer",
+    precio: 5000,
+    ver: "ver",
+  },
+];
 
+export const ProductsConsolas = [
+  {
+    id: 7,
+    img: "https://m.media-amazon.com/images/I/61zjj2sgXML.jpg",
+    nombre: "Xbox",
+    precio: 240,
+    ver: "ver",
+  },
+  {
+    id: 8,
+    img: "https://d500.epimg.net/cincodias/imagenes/2020/06/12/gadgets/1591947469_905660_1591947750_sumario_normal.jpg",
+    nombre: "Psp",
+    precio: 460,
+    ver: "ver",
+  },
+  {
+    id: 9,
+    img: "https://applauss.com/wp-content/uploads/2016/06/Nintendo-64-juegos.jpg",
+    nombre: "Nintendo",
+    precio: 3500,
+    ver: "ver",
+  },
+  {
+    id: 10,
+    img: "https://static.wixstatic.com/media/f68176_68197dd4a4644280b121886f04c01254~mv2.png/v1/fill/w_622,h_514,al_c/f68176_68197dd4a4644280b121886f04c01254~mv2.png",
+    nombre: "Pc gamer",
+    precio: 1000,
+    ver: "ver",
+  },
+  {
+    id: 11,
+    img: "https://fs-prod-cdn.nintendo-europe.com/media/images/08_content_images/systems_5/nintendo_switch_3/not_approved_1/NSwitch_ws_NintendoSwitch.png",
+    nombre: "Switch",
+    precio: 1000,
+    ver: "ver",
+  },
+  {
+    id: 12,
+    img: "https://ae01.alicdn.com/kf/Sbb3a800019e44e68b70e710665b1ec6da/MOBAPAD-controlador-de-consola-de-juegos-M6-Mando-de-consola-Gemini-para-Nintendo-Switch-mando-izquierdo.jpg_Q90.jpg_.webp",
+    nombre: "Moba",
+    precio: 5000,
+    ver: "ver",
+  },
+];
 
+export const ProductsDiversos = [
+  {
+    id: 13,
+    img: "https://www.pngplay.com/wp-content/uploads/7/Short-Hdmi-Cable-Transparent-File.png",
+    nombre: "Cable",
+    precio: 240,
+    ver: "ver",
+  },
+  {
+    id: 14,
+    img: "https://i.blogs.es/6ba656/lans/450_1000.jpg",
+    nombre: "Router",
+    precio: 460,
+    ver: "ver",
+  },
+  {
+    id: 15,
+    img: "https://resource.logitechg.com/content/dam/gaming/en/products/g335/g335-white-gallery-1.png",
+    nombre: "Audifonos",
+    precio: 3500,
+    ver: "ver",
+  },
+  {
+    id: 16,
+    img: "https://www.xtremetecpc.com/wp-content/uploads/2017/03/ada_tp-link_re450-a.png",
+    nombre: "Repetidor",
+    precio: 1000,
+    ver: "ver",
+  },
+  {
+    id: 17,
+    img: "https://consumer.huawei.com/content/dam/huawei-cbg-site/latam/mx/mkt/pdp/accessories/selfie-stick-pro/3840/web2x_01.jpg",
+    nombre: "Selfie stick",
+    precio: 1000,
+    ver: "ver",
+  },
+  {
+    id: 18,
+    img: "https://cdn.shopify.com/s/files/1/0599/0537/6406/products/D_805350-MLM46613593814_072021-O.jpg?v=1642515701",
+    nombre: "Funko",
+    precio: 5000,
+    ver: "ver",
+  },
+];
 
-  export const ProductsConsolas = [
-    {
-      id:7,
-      img: "https://m.media-amazon.com/images/I/61zjj2sgXML.jpg",
-      nombre: "Xbox",
-      precio: 240,
-      ver: "ver",
-    },
-    {
-      id:8,
-      img: "https://d500.epimg.net/cincodias/imagenes/2020/06/12/gadgets/1591947469_905660_1591947750_sumario_normal.jpg",
-      nombre: "Psp",
-      precio: 460,
-      ver: "ver",
-    },
-    {
-      id:9,
-      img: "https://applauss.com/wp-content/uploads/2016/06/Nintendo-64-juegos.jpg",
-      nombre: "Nintendo",
-      precio: 3500,
-      ver: "ver",
-    },
-    {
-      id:10,
-      img: "https://static.wixstatic.com/media/f68176_68197dd4a4644280b121886f04c01254~mv2.png/v1/fill/w_622,h_514,al_c/f68176_68197dd4a4644280b121886f04c01254~mv2.png",
-      nombre: "Pc gamer",
-      precio: 1000,
-      ver: "ver",
-    },
-    {
-      id:11,
-      img: "https://fs-prod-cdn.nintendo-europe.com/media/images/08_content_images/systems_5/nintendo_switch_3/not_approved_1/NSwitch_ws_NintendoSwitch.png",
-      nombre: "Switch",
-      precio: 1000,
-      ver: "ver",
-    },
-    {
-      id:12,
-      img: "https://ae01.alicdn.com/kf/Sbb3a800019e44e68b70e710665b1ec6da/MOBAPAD-controlador-de-consola-de-juegos-M6-Mando-de-consola-Gemini-para-Nintendo-Switch-mando-izquierdo.jpg_Q90.jpg_.webp",
-      nombre: "Moba",
-      precio: 5000,
-      ver: "ver",
-    },
-  ];
-
-  export const ProductsDiversos = [
-    {
-      id:13,
-      img: "https://www.pngplay.com/wp-content/uploads/7/Short-Hdmi-Cable-Transparent-File.png",
-      nombre: "Cable",
-      precio: 240,
-      ver: "ver",
-    },
-    {
-      id:14,
-      img: "https://i.blogs.es/6ba656/lans/450_1000.jpg",
-      nombre: "Router",
-      precio: 460,
-      ver: "ver",
-    },
-    {
-      id:15,
-      img: "https://resource.logitechg.com/content/dam/gaming/en/products/g335/g335-white-gallery-1.png",
-      nombre: "Audifonos",
-      precio: 3500,
-      ver: "ver",
-    },
-    {
-      id:16,
-      img: "https://www.xtremetecpc.com/wp-content/uploads/2017/03/ada_tp-link_re450-a.png",
-      nombre: "Repetidor",
-      precio: 1000,
-      ver: "ver",
-    },
-    {
-      id:17,
-      img: "https://consumer.huawei.com/content/dam/huawei-cbg-site/latam/mx/mkt/pdp/accessories/selfie-stick-pro/3840/web2x_01.jpg",
-      nombre: "Selfie stick",
-      precio: 1000,
-      ver: "ver",
-    },
-    {
-      id:18,
-      img: "https://cdn.shopify.com/s/files/1/0599/0537/6406/products/D_805350-MLM46613593814_072021-O.jpg?v=1642515701",
-      nombre: "Funko",
-      precio: 5000,
-      ver: "ver",
-    },
-  ];
+/* esta API REST sera probado con React
+export async function getAllProducts() {
+  try {
+    const datos = await fetch("https://fakestoreapi.com/products");
+    const date = await datos.json();
+    console.log(date);
+    return date;
+  } catch (error) {
+    console.error(error);
+  }
+}
+*/
