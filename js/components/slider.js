8a7fd24e19407195ba024cdeab4ba4e239021d44
unknown <ing.ramirozs@gmail.com>
2022-11-27 21:40:14 -0600

    create function slider

---
+++ js/components/slider.js
@@ -0,0 +1,23 @@
+export const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+ 
+});
