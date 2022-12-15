5099e541921144722eb7c575d6941e13e967f522
unknown <ing.ramirozs@gmail.com>
2022-12-14 22:48:24 -0600

    add function for sidebar

--- js/script.js
+++ js/script.js
@@ -11,11 +11,9 @@ const Items_gadgets = document.querySelector(".Items_gadgets");
 const Items_consolas = document.querySelector(".Items_consolas");
 const Items_diversos = document.querySelector(".Items_diversos");
 
-
 DisplayGadgets(Items_gadgets);
 DisplayConsolas(Items_consolas);
 DisplayDiversos(Items_diversos);
-
 
 const input = document.querySelector(".search_input");
 input.addEventListener("input", ChangeItem);
@@ -31,7 +29,22 @@ elementsItems.forEach((elements) => {
   elements.lastChild.addEventListener("click", ClickProduct);
 });
 
-const btnlogin = document.querySelector(".btnlogin")
-btnlogin.addEventListener("click" ,() => location.href = "login.html");
+const btnlogin = document.querySelector(".btnlogin");
+btnlogin.addEventListener("click", () => (location.href = "login.html"));
+
+const btnsidebar = document.querySelector(".button_sidebar");
+const closesidebar = document.querySelector(".close_sidebar");
+
+function openSlideMenu() {
+  document.querySelector(".container_sidebar").style.marginLeft = "-180px";
+  document.querySelector("aside").style.display = "flex";
+}
+function closeSlideMenu() {
+  document.querySelector(".container_sidebar").style.marginLeft = "180px";
+  document.querySelector(".header_movil").style.display = "block";
+}
+
+closesidebar.addEventListener("click", closeSlideMenu);
+btnsidebar.addEventListener("click", openSlideMenu);
 
 swiper();
